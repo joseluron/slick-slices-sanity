ac0a38223ca10a7605896b667a2cdd76f8414ad3
luron <joseluron@gmail.com>
2020-12-26 01:32:32 +0100

    First commit with starter sanity cms project

---
+++ schemas/topping.js
@@ -0,0 +1,36 @@
+import { FaPepperHot as icon } from 'react-icons/fa';
+
+export default {
+  // Computer name
+  name: 'topping',
+
+  // Visible name
+  title: 'Toppings',
+  type: 'document',
+  icon,
+  fields: [
+    {
+      name: 'name',
+      title: 'Topping name',
+      type: 'string',
+      description: 'Name of the topping',
+    },
+    {
+      name: 'vegetarian',
+      title: 'Vegetarian',
+      type: 'boolean',
+      options: {
+        layout: 'checkbox',
+      },
+    },
+  ],
+  preview: {
+    select: {
+      name: 'name',
+      vegetarian: 'vegetarian',
+    },
+    prepare: (fields) => ({
+      title: `${fields.name} ${fields.vegetarian ? 'G4U' : ''}`,
+    }),
+  },
+};
